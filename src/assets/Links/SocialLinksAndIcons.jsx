967adaf0f6f815cfb3298989952f3a89c87eac7d
Shubham Thorve <shubhamthorve531@gmail.com>
2025-01-26 19:00:02 +0530

    added typed social links and skills section

---
+++ src/assets/Links/SocialLinksAndIcons.jsx
@@ -0,0 +1,25 @@
+import { IconButton } from "@mui/material";
+import { LinkedIn, GitHub, Twitter, Article } from "@mui/icons-material";
+
+export const socialLinks = [
+  {
+    href: "https://www.linkedin.com/in/shubhamthorve",
+    color: "#0A66C2",
+    icon: <LinkedIn fontSize="large" />,
+  },
+  {
+    href: "https://github.com/shubhamthorve531",
+    color: "black",
+    icon: <GitHub fontSize="large" />,
+  },
+  {
+    href: "https://twitter.com/shubhamthorve3",
+    color: "#1DA1F2",
+    icon: <Twitter fontSize="large" />,
+  },
+  {
+    href: "https://medium.com/@shubhamthorve531",
+    color: "black",
+    icon: <Article fontSize="large" />,
+  },
+];
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,17 @@
 import React from "react";
-import { Container, Typography, Box } from "@mui/material";
+import { Container, Typography, Box, IconButton, Grid } from "@mui/material";
+import { LinkedIn, GitHub, Twitter, Article } from "@mui/icons-material";
+import { FaHtml5, FaCss3Alt, FaJs, FaReact, FaDatabase, FaMicrosoft } from "react-icons/fa";
+import { SiTypescript, SiMicrosoftazure, SiVisualstudiocode, SiVisualstudio,SiCsharp, SiMicrosoft } from "react-icons/si";
 import myImage from "../assets/Images/myImage1.jpg";
 import handWave from "../assets/Images/hand-wave.svg";
 import TypingText from "../components/TypingText";
 
+import { socialLinks } from "../assets/Links/SocialLinksAndIcons";
+
 const Home = () => {
   return (
+    <>
     <Container
       sx={{
         display: "flex",
@@ -16,22 +22,44 @@ const Home = () => {
         mt: 5,
       }}
     >
-      {/* Profile Image */}
-      <Box>
+      {/* Profile Image and Social Links */}
+      <Box sx={{ textAlign: "center" }}>
         <img
           src={myImage}
           alt="Shubham Thorve"
           style={{
-            borderRadius: "50%",
+            borderRadius: "20%",
             height: "250px",
             width: "250px",
             objectFit: "cover",
           }}
         />
+        
+        <Typography
+          variant="h6"
+          sx={{ mt: 2, fontWeight: "bold", color: "grey" }}
+        >
+          You can find me on
+        </Typography>
+
+        <Box sx={{ display: "flex", justifyContent: "center", gap: 2, mt: 1 }}>
+      {socialLinks.map((link, index) => (
+        <IconButton
+          key={index}
+          component="a"
+          href={link.href}
+          target="_blank"
+          rel="noopener noreferrer"
+          sx={{ color: link.color }}
+        >
+          {link.icon}
+        </IconButton>
+      ))}
+    </Box>
       </Box>
 
       {/* Text Content */}
-      <Box sx={{ ml: { md: 4 }, mt: { xs: 3, md: 0 }, textAlign: "left" }}>
+      <Box sx={{ ml: { md: 4 }, mt: { xs: 3, md: 0 }, textAlign: "left", paddingLeft: "20px" }}>
         <Box sx={{ display: "flex", alignItems: "center", gap: 1 }}>
           <Typography variant="h5" sx={{ color: "black", opacity: 0.6 }}>
             Hey, there
@@ -81,14 +109,50 @@ const Home = () => {
             fontSize: { xs: "20px", sm: "24px", md: "28px" },
           }}
         >
-          Passionate Frontend Developer | Backend and Cloud Enthusiast | Blogger from Pune, India.
+          Passionate Frontend Developer | Backend, Cloud and AI Enthusiast | From Pune, India.
         </Typography>
 
-        {/* <Box sx={{ mt: 2 }}>
+        <Box sx={{ mt: 2 }}>
           <TypingText />
-        </Box> */}
+        </Box>
       </Box>
     </Container>
+    {/* Skills Section */}
+    <Container sx={{ mt: 5, textAlign: "left" }}>
+  <Typography
+    variant="h6"
+    sx={{ fontWeight: "bold", color: "grey", textAlign: "left" }}
+  >
+    Skills & Technologies
+  </Typography>
+  <Grid container spacing={2} justifyContent="space-between" mt={1}>
+    {[
+      { name: "HTML", icon: <FaHtml5 color="#E34F26" size={30} /> },
+      { name: "CSS", icon: <FaCss3Alt color="#1572B6" size={30} /> },
+      { name: "JavaScript", icon: <FaJs color="#F7DF1E" size={30} /> },
+      { name: "TypeScript", icon: <SiTypescript color="#3178C6" size={30} /> },
+      { name: "React", icon: <FaReact color="#61DAFB" size={30} /> },
+      { name: ".NET", icon: <FaMicrosoft color="#512BD4" size={30} /> },
+      { name: "C#", icon: <SiCsharp color="#239120" size={30} /> },
+      { name: "Fluent UI", icon: <SiMicrosoft color="#0078D4" size={30} /> },
+      { name: "Azure", icon: <SiMicrosoftazure color="#0089D6" size={30} /> },
+      { name: "SQL Server", icon: <FaDatabase color="#CC2927" size={30} /> },
+      { name: "VS Code", icon: <SiVisualstudiocode color="#007ACC" size={30} /> },
+      { name: "Visual Studio", icon: <SiVisualstudio color="#5C2D91" size={30} /> },
+    ].map((skill, index) => (
+      <Grid item xs={6} sm={4} md={1} key={index}>
+        <Box sx={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
+          {skill.icon}
+          <Typography variant="body2" sx={{ mt: 1, lineHeight: 1,fontSize: "0.8rem" }}>
+            {skill.name}
+          </Typography>
+        </Box>
+      </Grid>
+    ))}
+  </Grid>
+</Container>
+
+  </>
   );
 };
 
